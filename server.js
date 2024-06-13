bee570782d57ba8d89613a505647f199d29393e0
Entrpix <korr626@outlook.com>
2024-06-13 16:58:45 -0400

    actually fix ssl

--- server.js
+++ server.js
@@ -1,54 +1,52 @@
 import createRammerhead from 'rammerhead/src/server/index.js';
 import { createBareServer } from '@tomphttp/bare-server-node';
 import { epoxyPath } from '@mercuryworkshop/epoxy-transport';
-import config from './config.json' assert { type: 'json' };
 import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import { baremuxPath } from '@mercuryworkshop/bare-mux';
-import { fileURLToPath } from 'node:url';
-import { readFileSync } from 'node:fs';
 import { dirname } from 'node:path';
 import wisp from 'wisp-server-node';
 import express from 'express';
+import https from 'https';
 import http from 'http';
-
+import fs from 'node:fs';
 
 const bare = createBareServer('/bare/');
 const rh = createRammerhead();
 
 const rammerheadScopes = [
-  '/rammerhead.js',
-  '/hammerhead.js',
-  '/transport-worker.js',
-  '/task.js',
-  '/iframe-task.js',
-  '/worker-hammerhead.js',
-  '/messaging',
-  '/sessionexists',
-  '/deletesession',
-  '/newsession',
-  '/editsession',
-  '/needpassword',
-  '/syncLocalStorage',
-  '/api/shuffleDict',
-  '/mainport'
+    '/rammerhead.js',
+    '/hammerhead.js',
+    '/transport-worker.js',
+    '/task.js',
+    '/iframe-task.js',
+    '/worker-hammerhead.js',
+    '/messaging',
+    '/sessionexists',
+    '/deletesession',
+    '/newsession',
+    '/editsession',
+    '/needpassword',
+    '/syncLocalStorage',
+    '/api/shuffleDict',
+    '/mainport'
 ];
 
 const rammerheadSession = /^\/[a-z0-9]{32}/;
 
 function shouldRouteRh(req) {
-  const url = new URL(req.url, 'http://0.0.0.0');
-  return (
-    rammerheadScopes.includes(url.pathname) ||
-    rammerheadSession.test(url.pathname)
-  );
+    const url = new URL(req.url, 'http://0.0.0.0');
+    return (
+        rammerheadScopes.includes(url.pathname) ||
+        rammerheadSession.test(url.pathname)
+    );
 }
 
 function routeRhRequest(req, res) {
-  rh.emit('request', req, res);
+    rh.emit('request', req, res);
 }
 
 function routeRhUpgrade(req, socket, head) {
-  rh.emit('upgrade', req, socket, head);
+    rh.emit('upgrade', req, socket, head);
 }
 
 const server = http.createServer((req, res) => {
@@ -86,18 +84,26 @@ app.use((req, res, next) => {
     res.sendFile('public/pages/error/404.html', { root: dirname(fileURLToPath(import.meta.url)) });
 });
 
-function readSsl(key, cert) {
-    readFileSync(key);
-    readFileSync(cert);
-};
+let port = 3000;
 
-let port = config.port;
-
-if (config.ssl === true) {
-    readSsl(config.ssl_opt.key, config.ssl_opt.cert);
-};
-
+// For SSL comment all lines below:
 server.on('listening', () => {
     console.log(`Server listening on port: ${port}`);
 });
 server.listen(port);
+
+// For SSL uncomment the lines below:
+/*
+const sslOptions = {
+    key: fs.readFileSync(''), // Path to private key
+    cert: fs.readFileSync('') // Path to certificate
+};
+port = 3000;
+https.createServer(sslOptions, app).listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+http.createServer((req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
+*/
