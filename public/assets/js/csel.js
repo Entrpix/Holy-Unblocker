0c9a1002d2060767c0067080743842ce14154edf
Entrpix <korr626@outlook.com>
2024-06-12 23:05:04 -0400

    mrrp mrow?

--- public/assets/js/csel.js
+++ public/assets/js/csel.js
@@ -52,31 +52,22 @@
         setCookie("HBIcon", value);
     }
 
-    function pageHideAds() {
-        document.querySelectorAll(".ad").forEach(n => n.style.display = "none");
-    }
-
-    function pageShowAds() {
-        document.querySelectorAll(".ad").forEach(n => n.style.display = "block");
-    }
-
-    function hideAds() {
-        pageHideAds();
-        setCookie("HBHideAds", "true");
-    }
-
-    function showAds() {
-        pageShowAds();
-        removeCookie("HBHideAds");
-    }
-
     readCookie("HBTitle").then(s => (s != "undefined") && pageTitle(s));
     readCookie("HBIcon").then(s => (s != "undefined") && pageIcon(s));
 
-    readCookie("HBHideAds").then(s => (s != "undefined") && (function() { pageHideAds(); (document.getElementById("hideads") || {}).checked = "true"; })());
-
     if (csel) {
-        csel.innerHTML = decodeURIComponent(atob("JTNDcCUyMGNsYXNzJTNEJTIyY3NlbHRpdGxlJTIyJTNFVGFiJTIwQ2xvYWslM0MlMkZwJTNFJTBBJTNDcCUyMGNsYXNzJTNEJTIyY3NlbGxhYmVsJTIyJTNFQ2hhbmdlJTIwdGhlJTIwdGl0bGUlM0ElM0MlMkZwJTNFJTBBJTNDZm9ybSUyMGNsYXNzJTNEJTIyY3NlbGZvcm0lMjIlMjBpZCUzRCUyMnRpdGxlZm9ybSUyMiUzRSUwQSUyMCUyMCUyMCUyMCUzQ2lucHV0JTIwdHlwZSUzRCUyMnRleHQlMjIlMjBwbGFjZWhvbGRlciUzRCUyMlRhYiUyMFRpdGxlJTIyJTIwc3BlbGxjaGVjayUzRCUyMmZhbHNlJTIyJTNFJTNDaW5wdXQlMjBjbGFzcyUzRCUyMmNzZWxidXR0b24lMjIlMjB0eXBlJTNEJTIyc3VibWl0JTIyJTIwdmFsdWUlM0QlMjJBcHBseSUyMiUzRSUwQSUzQyUyRmZvcm0lM0UlMEElM0NwJTIwY2xhc3MlM0QlMjJjc2VsbGFiZWwlMjIlM0VDaGFuZ2UlMjB0aGUlMjAlM0NhJTIwaHJlZiUzRCUyMiUyRiUzRmklMjIlM0VpY29uJTNDJTJGYSUzRSUzQSUzQyUyRnAlM0UlMEElM0Nmb3JtJTIwY2xhc3MlM0QlMjJjc2VsZm9ybSUyMiUyMGlkJTNEJTIyaWNvbmZvcm0lMjIlM0UlMEElMjAlMjAlMjAlMjAlM0NpbnB1dCUyMHR5cGUlM0QlMjJ0ZXh0JTIyJTIwcGxhY2Vob2xkZXIlM0QlMjJJY29uJTIwVVJMJTIyJTIwc3BlbGxjaGVjayUzRCUyMmZhbHNlJTIyJTNFJTNDaW5wdXQlMjBjbGFzcyUzRCUyMmNzZWxidXR0b24lMjIlMjB0eXBlJTNEJTIyc3VibWl0JTIyJTIwdmFsdWUlM0QlMjJBcHBseSUyMiUzRSUwQSUzQyUyRmZvcm0lM0UlMEElM0NpbnB1dCUyMGlkJTNEJTIyY3NlbHJlc2V0JTIyJTIwY2xhc3MlM0QlMjJjc2VsYnV0dG9uJTIyJTIwdHlwZSUzRCUyMmJ1dHRvbiUyMiUyMHZhbHVlJTNEJTIyUmVzZXQlMjIlM0UlMEElM0NwJTIwY2xhc3MlM0QlMjJjc2VsbGFiZWwlMjIlM0UlMEElMjAlMjAlMjAlMjAlM0NpbnB1dCUyMGlkJTNEJTIyaGlkZWFkcyUyMiUyMHR5cGUlM0QlMjJjaGVja2JveCUyMiUzRSUwQSUyMCUyMCUyMCUyMCUzQ3NwYW4lM0VIaWRlJTIwQWRzJTNDJTJGc3BhbiUzRSUwQSUzQyUyRnAlM0UlMEElM0NwJTNFQWRzJTIwaGVscCUyMHN1cHBvcnQlMjBIJTI2JTIzMTczJTNCbyUyNiUyMzE3MyUzQmx5JTIwVSUyNiUyMzE3MyUzQm5iJTI2JTIzMTczJTNCbG9jayUyNiUyMzE3MyUzQmVyISUzQyUyRnAlM0U="));
+        csel.innerHTML = `
+<p class="cseltitle">Tab Cloak</p>
+<p class="csellabel">Change the title:</p>
+<form class="cselform" id="titleform">
+    <input type="text" placeholder="Tab Title" spellcheck="false"><input class="cselbutton" type="submit" value="Apply">
+</form>
+<p class="csellabel">Change the icon:</p>
+<form class="cselform" id="iconform">
+    <input type="text" placeholder="Icon URL" spellcheck="false"><input class="cselbutton" type="submit" value="Apply">
+</form>
+<input id="cselreset" class="cselbutton" type="button" value="Reset">
+        `;
 
         document.getElementById("titleform").addEventListener("submit", function(e) {
             e.preventDefault();
@@ -105,10 +96,6 @@
                 pageTitle("H&shy;o&shy;ly Un&shy;blo&shy;ck&shy;er");
                 pageIcon("assets/img/icon.png");
             }
-        }, false);
-
-        document.getElementById("hideads").addEventListener("change", function(e) {
-            e.target.checked ? hideAds() : showAds();
         }, false);
     }
 })();
--- public/assets/js/register-sw.js
+++ public/assets/js/register-sw.js
@@ -1,17 +1,20 @@
 const stockSW = "/uv/sw.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
+
 async function registerSW() {
-  if (
-    location.protocol !== "https:" &&
-    !swAllowedHostnames.includes(location.hostname)
-  )
-    throw new Error("Service workers cannot be registered without https.");
+  if (!navigator.serviceWorker) {
+    if (
+      location.protocol !== "https:" &&
+      !swAllowedHostnames.includes(location.hostname)
+    )
+      throw new Error("Service workers cannot be registered without https.");
 
-  if (!navigator.serviceWorker)
     throw new Error("Your browser doesn't support service workers.");
+  }
 
-  await navigator.serviceWorker.register(stockSW, {
-    scope: __uv$config.prefix,
-  });
+  await navigator.serviceWorker.register(stockSW);
+
+  let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+  await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
 }
